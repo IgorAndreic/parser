521ee28e7179ba9bb6f1e73b248a2dd92803ff5f
Marsh_Meg <uraevdmitrij031@gmail.com>
2024-07-07 13:27:06 +0400

    добавил вывод инфы

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -2,6 +2,48 @@ import {useParams} from "react-router-dom";
 import axiosInstance from "../api/axiosInstance"
 import {useState, useEffect} from "react";
 
+function DataObj({data}) {
+  let data_out = ""
+  if (typeof data == "object" && data != null) {
+    if (Array.isArray(data)) {
+      console.log(data)
+      for (let i in Object.values(data)) {
+        if (typeof data[i] == "object" && data[i] != null) {
+          const vals = Object.values(data[i])
+          for (let i in Object.keys(vals)) {
+            data_out += `${(vals[i])}\n`
+          }
+        }
+      }
+    } else {
+      const vals = Object.values(data)
+      for (let i in Object.keys(vals)) {
+        data_out += `${(vals[i])}\n`
+      }
+    }
+  } else {
+    data_out = JSON.stringify(data)
+  }
+
+  return (
+    <th>
+      {data_out}
+    </th>
+  )
+}
+
+function DataLine({line}) {
+  console.log(line)
+  return (
+    <tr>
+      {
+        line &&
+        Object.values(line).map((data, key) => <DataObj key={key} data={data}/>)
+      }
+    </tr>
+  )
+}
+
 export default function DataTable() {
   const { table_name } = useParams()
   const [data, setData] = useState({})
@@ -33,17 +75,10 @@ export default function DataTable() {
           </tr>
         </thead>
         <tbody>
-          {/*{*/}
-          {/*  data.results.map((item, index) => (*/}
-          {/*    <tr key={index}>*/}
-          {/*      {*/}
-          {/*        Object.values(item).map((value, i) => (*/}
-          {/*          <td key={i}>{value}</td>*/}
-          {/*        ))*/}
-          {/*      }*/}
-          {/*    </tr>*/}
-          {/*  ))*/}
-          {/*}*/}
+          {
+            data.results &&
+            Object.values(data.results).map((line, id) => <DataLine key={id} line={line} />)
+          }
         </tbody>
       </table>
     </>
